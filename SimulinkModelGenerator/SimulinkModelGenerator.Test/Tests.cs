e3cf1f3376e2785a919635c7e259b09926cff7c0
ledjon-behluli <behluliledjon@hotmail.com>
2020-05-16 14:55:49 +0200

    images & new test

--- SimulinkModelGenerator/SimulinkModelGenerator.Test/Tests.cs
+++ SimulinkModelGenerator/SimulinkModelGenerator.Test/Tests.cs
@@ -166,5 +166,37 @@ namespace SimulinkModelGenerator.Test
                 })
                 .Save(path);
         }
+
+        [Test]
+        public void Test5()
+        {
+            new ModelBuilder()
+                .WithName("test5")
+                .AddControlSystem(cs =>
+                {
+                    cs.AddSources(s => s.AddStep(sp => sp.SetStepTime(0).SetPosition(190, 145))
+                                        .AddConstant(c => c.SetPosition(190, 245)));
+                    cs.AddMathOperations(mo => mo.AddSum(sum => sum.SetInputs(InputType.Plus, InputType.Minus).SetPosition(320, 150))
+                                                .AddGain(g => g.SetGain(1).FlipHorizontally().SetPosition(515, 230)));
+                    cs.AddContinuous(co =>
+                    {
+                        co.AddPIDController(pid => pid.SetProportional(31.0019358281379)
+                                                      .SetIntegral(88.4489521692078)
+                                                      .SetDerivative(1.81032163065042)
+                                                      .SetFilterCoefficient(4337.28406726102)
+                                                      .SetPosition(435, 142));
+                        co.AddTransferFunction(tf => tf.SetNumerator(20).SetDenominator(1, 10, 20).SetPosition(595, 142));
+                    });
+                    cs.AddSinks(s => s.AddScope(scope => scope.SetInputPorts(2).SetPosition(820, 144)));
+                    cs.AddConnections("Step", c =>
+                    {
+                        c.ThanConnect("Sum").ThanConnect("PID Controller").ThanConnect("TransferFcn")
+                        .Branch(b => b.Towards("Scope", 1))
+                        .Branch(b => b.Towards("Gain").ThanConnect("Sum", 2))
+                        .Connect("Constant", "Scope", 1, 2);
+                    });
+                })
+                .Save(path);
+        }
     }
 }
